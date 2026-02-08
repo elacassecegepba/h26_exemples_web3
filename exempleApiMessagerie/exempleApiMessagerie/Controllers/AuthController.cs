2fbb184aab62f7e4d820bc073f18a1d5b1725d87
elacassecegepba <114033700+elacassecegepba@users.noreply.github.com>
2026-02-08 18:31:34 -0500

    Register + Login

---
+++ exempleApiMessagerie/exempleApiMessagerie/Controllers/AuthController.cs
@@ -0,0 +1,92 @@
+﻿using exempleApiMessagerie.Models;
+using exempleApiMessagerie.Models.Utilisateurs;
+using exempleApiMessagerie.SQLite;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exempleApiMessagerie.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase {
+    private readonly AppDbContext _context;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(AppDbContext context, IConfiguration configuration) {
+        _context = context;
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Crée un nouvel utilisateur.
+    /// </summary>
+    /// <param name="utilisateurDTO">L'utilisateur à créer.</param>
+    /// <returns>Le nouvel utilisateur créé.</returns>
+    /// <response code="200">Le nouvel utilisateur a été créé avec succès.</response>
+    /// <response code="400">Les données fournies sont invalides.</response>
+    /// <response code="409">Conflit en cas de violation de contrainte unique.</response>
+    [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status409Conflict, MediaTypeNames.Application.ProblemJson)]
+    public async Task<ActionResult> Register(UtilisateurUpsertDTO utilisateurDTO) {
+        Utilisateur nouvelUtilisateur = Utilisateur.FromDTO(utilisateurDTO);
+        _context.Utilisateurs.Add(nouvelUtilisateur);
+        try {
+            await _context.SaveChangesAsync();
+        } catch (DbUpdateException e) when (SQLiteHelper.EstErreurContrainteUnique(e)) {
+            return Conflict(new ProblemDetails {
+                Detail = "Email ou nom d'utilisateur déjà utilisé."
+            });
+        }
+
+        return Ok();
+    }
+
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UtilisateurLoginDTO loginDTO) {
+        Utilisateur? utilisateur = await _context.Utilisateurs
+            .Where(u => u.Email == loginDTO.Email)
+            .FirstOrDefaultAsync();
+
+        if (utilisateur == null || !utilisateur.VerifierMotDePasse(loginDTO.MotDePasse)) {
+            return Unauthorized();
+        }
+
+        return Ok(CreerToken(utilisateur));
+    }
+
+    private string CreerToken(Utilisateur utilisateur) {
+        List<Claim> claims = new() {
+            new Claim("UserId", utilisateur.Id.ToString())
+        };
+
+        // TokenPassKey doit être défini dans appsettings.json ou dans les variables d'environnement
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_configuration["TokenPassKey"]!) 
+        );
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+}
---
+++ exempleApiMessagerie/exempleApiMessagerie/Models/Utilisateurs/UtilisateurLoginDTO.cs
@@ -0,0 +1,32 @@
+﻿using exempleApiMessagerie.Models.Messages;
+using Konscious.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+
+namespace exempleApiMessagerie.Models.Utilisateurs;
+
+/// <summary>
+/// Représente un utilisateur.
+/// Utilisée par les endpoints d'authentification pour le login, elle contient le mot de passe en clair pour permettre la vérification du mot de passe lors du login.
+/// </summary>
+public class UtilisateurLoginDTO {
+    /// <summary>
+    /// Le courriel de l'utilisateur.
+    /// </summary>
+    /// <example>jean.dupont@test.com</example>
+    [EmailAddress]
+    [MaxLength(255)]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// Le mot de passe de l'utilisateur.
+    /// </summary>
+    /// <example>Password1!</example>
+    [MinLength(3)]
+    [MaxLength(255)]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d).*$", ErrorMessage = "Au moins une lettre et un nombre")]
+    public required string MotDePasse { get; set; }
+
+}
